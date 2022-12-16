bf37d8c0ae9e5dc10111cd8e8538d52f309981bc
alilakhani786 <73431376+alilakhani786@users.noreply.github.com>
2022-12-16 19:11:22 +0800

    testing...

--- templates/onboarding/akamai-delivery/versions.tf
+++ templates/onboarding/akamai-delivery/versions.tf
@@ -9,7 +9,8 @@ terraform {
   required_providers {
     akamai = {
       source = "akamai/akamai"
+      version = "3.1.0"
     }
   }
-  required_version = ">= 0.13"
+  #required_version = "3.1.0"
 }
